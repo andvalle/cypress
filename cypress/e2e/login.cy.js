616d0d97b62f8b999d9ca50084b98be4688f3fc4
Andrew Valle <Andrew.Valle@br.proveedor-mcd.com>
2025-09-05 15:30:50 -0300

    Primeiro commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,18 @@
+describe('Orange HRM Testes ', () => {
+  it('Login Sucess', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Admin')
+    cy.get("[name='password']").type('admin123')
+    cy.get("[type='submit']").click()
+    cy.location('pathname').should('equal' , '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+  })
+
+  it('Login Fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Teste')
+    cy.get("[name='password']").type('Teste')
+    cy.get("[type='submit']").click()
+    cy.get("[type='submit']")
+  })
+})
