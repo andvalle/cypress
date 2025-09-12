0a684e2e56cf82895e9b793ce734def11ea03a96
Andrew Valle <Andrew.Valle@br.proveedor-mcd.com>
2025-09-12 12:31:07 -0300

    Criado o parametro  userdata para guardar  usuarios e senhas

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,22 +4,30 @@ describe('Orange HRM Testes ', () => {
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
     selectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text",
-    wrongCredentialAlert: "[role='alert']"
+    wrongCredentialAlert: "[role='alert']",
+    checkPage: ".orangehrm-dashboard-grid"
   }
+ const userData = {
+      loginSucess: "Admin",
+      passSucess: "admin123",
+      loginFail: "teste",
+      passFail: "teste"
+ }
   
   it('Login Sucess', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.usernameField).type('Admin')
-    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.usernameField).type(userData.loginSucess)
+    cy.get(selectorList.passwordField).type(userData.passSucess)
     cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal' , '/web/index.php/dashboard/index')
-    cy.get(selectorList.selectionTitleTopBar).contains('Dashboard')
+    cy.get(selectorList.checkPage)
+    
   })
 
   it('Login Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.usernameField).type('Teste')
-    cy.get(selectorList.passwordField).type('Teste')
+    cy.get(selectorList.usernameField).type(userData.loginFail)
+    cy.get(selectorList.passwordField).type(userData.passFail)
     cy.get(selectorList.loginButton).click()
     cy.get(selectorList.wrongCredentialAlert)
   })
