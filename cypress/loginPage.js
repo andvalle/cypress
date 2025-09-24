50ca971d10642a9c2addc8038fad06959879a741
Andrew Valle <66800411+andvalle@users.noreply.github.com>
2025-09-24 10:30:05 -0300

    Update loginPage.js

--- cypress/loginPage.js
+++ cypress/loginPage.js
@@ -7,19 +7,16 @@ class loginPage {
             wrongCredentialAlert: "[role='alert']"
             }
         return seletores
-
         }
 
         loginSucess(username, password) {
-        cy.get(this.selectorList().usernameField).type(username)
-        cy.get(this.selectorList().passwordField).type(password)
-        cy.get(this.selectorList().loginButton).click()
+            cy.get(this.selectorList().usernameField).type(username)
+            cy.get(this.selectorList().passwordField).type(password)
+            cy.get(this.selectorList().loginButton).click()
         }
 
         loginIt(){
-        cy.visit('/auth/login')
-        }
-        
-
+            cy.visit('/auth/login')
+        }     
 }
   export default loginPage
