4e19d2c3ab50edfaf9bd5276e9e5e919e0e56ee6
Andrew Valle <Andrew.Valle@br.proveedor-mcd.com>
2025-09-25 15:26:00 -0300

    adicionando os funcionalidades no page object e melhorando o código

---
+++ cypress/dashboardPage.js
@@ -0,0 +1,68 @@
+// Define uma classe chamada "dashPage" para organizar seletores e métodos da página de Dashboard
+class dashPage {
+
+    // Método que retorna um objeto com todos os seletores da página
+    selectorList(){
+        const seletoresDashPage = {
+            checkPage: ".orangehrm-dashboard-grid", // Seletor que identifica se a página do Dashboard carregou
+            myinfoButton: "[href='/web/index.php/pim/viewMyDetails']", // Botão "My Info" no menu
+            firstName: "[name='firstName']", // Campo de texto para o primeiro nome
+            middleName: "[name='middleName']", // Campo de texto para o nome do meio
+            lastName: "[placeholder='Last Name']", // Campo de texto para o último nome
+            genericField: ".oxd-input--active", // Campos genéricos de input (usado com .eq para selecionar um específico)
+            dataField: "[placeholder='yyyy-dd-mm']", // Campo de data
+            selectArrow: ".oxd-select-text--arrow", // Ícone de seta para dropdowns
+            dateCloseButton: ".--close", // Botão para fechar o seletor de data
+            saveButton: "[type='submit']", // Botão de salvar
+            toastButton: ".oxd-toast", // Mensagem de confirmação (toast)
+            tabIndex: "[tabindex='0']" // Elemento com tabindex, seleciona o pais e o sexo.
+        }
+       return seletoresDashPage // Retorna todos os seletores
+    }
+    
+    // Método para validar que o usuário está no Dashboard e acessar o menu "My Info"
+    checkDashBoard(){
+        cy.location('pathname').should('equal' , '/web/index.php/dashboard/index') 
+        // Confirma que a URL contém o caminho do Dashboard
+
+        cy.get(this.selectorList().checkPage) 
+        // Valida que o elemento principal do Dashboard existe
+
+        cy.get(this.selectorList().myinfoButton).click()
+        // Clica no botão "My Info" para abrir a página de informações do usuário
+    }
+
+    // Método para preencher e enviar o formulário "My Info"
+    insertForm(){
+        // Limpa e insere valores nos campos de nome
+        cy.get(this.selectorList().firstName).clear().type("TestName")
+        cy.get(this.selectorList().middleName).clear().type("MiddleNameTest")
+        cy.get(this.selectorList().lastName).clear().type("LastNameTest")
+
+        // Preenche campos genéricos usando índices para selecionar a posição correta
+        cy.get(this.selectorList().genericField).eq(3).clear().type("2223334445") 
+        cy.get(this.selectorList().genericField).eq(4).clear().type("432122")
+        cy.get(this.selectorList().genericField).eq(5).clear().type("4354335675489")
+
+        // Preenche o campo de data
+        cy.get(this.selectorList().dataField).eq(0).clear().type("2025-12-31")
+        cy.get(this.selectorList().dateCloseButton).click() // Fecha o calendário
+
+        // Salva as informações
+        cy.get(this.selectorList().saveButton).eq(0).click()
+
+        // Valida que apareceu a mensagem de sucesso
+        cy.get('.oxd-toast').should('contain', "Successfully Updated")
+
+        // Interage com abas adicionais usando tabindex e cliques forçados
+        cy.get(this.selectorList().tabIndex).eq(0).click({force:true})
+        cy.get(':nth-child(4) > span').click({force:true})
+
+        cy.get(this.selectorList().tabIndex).eq(1).click({force:true})
+        cy.get(':nth-child(3) > span').click({force:true})
+    }
+    
+}
+
+// Exporta a classe para ser usada em outros arquivos de teste
+export default dashPage
--- cypress/e2e/userInfo.cy.js
+++ cypress/e2e/userInfo.cy.js
@@ -1,61 +1,52 @@
+import dashboardPage from '../dashboardPage'
 import userData from '../fixtures/users/userData.json'
 import loginPage from '../loginPage'
 
 const login = new loginPage()
+const dashboardCheck = new dashboardPage()
 
 describe('Orange HRM Testes ', () => {
-  const selectorList = {
-    selectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text",
-    checkPage: ".orangehrm-dashboard-grid",
-    myinfoButton: "[href='/web/index.php/pim/viewMyDetails']",
-    firstName: "[name='firstName']",
-    middleName: "[name='middleName']",
-    lastName: "[placeholder='Last Name']",
-    genericField: ".oxd-input--active",
-    dataField: "[placeholder='yyyy-dd-mm']",
-    selectArrow: ".oxd-select-text--arrow",
-    dateCloseButton: ".--close",
-    saveButton: "[type='submit']",
-    toastButton: ".oxd-toast",
-    tabIndex: "[tabindex='0']"
-    }
-it.only('User info update - check ', () => {
-   login.loginIt()
-   login.loginSucess(userData.userSuccess.loginSucess,userData.userSuccess.passSucess)
-    cy.location('pathname').should('equal' , '/web/index.php/dashboard/index')
-    cy.get(selectorList.checkPage)
-    cy.get(selectorList.myinfoButton).click()
-    cy.get(selectorList.firstName).clear().type("TestName")
-    cy.get(selectorList.middleName).clear().type("MiddleNameTest")
-    cy.get(selectorList.lastName).clear().type("LastNameTest")
-    cy.get(selectorList.genericField).eq(3).clear().type("2223334445")
-    cy.get(selectorList.genericField).eq(4).clear().type("432122")
-    cy.get(selectorList.genericField).eq(5).clear().type("4354335675489")
-    cy.get(selectorList.dataField).eq(0).clear().type("2025-12-31")
-    cy.get(selectorList.dateCloseButton).click()
-    cy.get(selectorList.saveButton).eq(0).click()
-    cy.get('.oxd-toast').should('contain', "Successfully Updated")
-    cy.get(selectorList.tabIndex).eq(0).click({force:true})
-    cy.get(':nth-child(4) > span').click({force:true})
-    cy.get(selectorList.tabIndex).eq(1).click({force:true})
-    cy.get(':nth-child(3) > span').click({force:true})
-    
-})
-  it('Login Sucess', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userSuccess.loginSucess)
-    cy.get(selectorList.passwordField).type(userData.userSuccess.passSucess)
-    cy.get(selectorList.loginButton).click()
-    cy.location('pathname').should('equal' , '/web/index.php/dashboard/index')
-    cy.get(selectorList.checkPage)
-    
-  })
+  // const selectorList = {
+  //   checkPage: ".orangehrm-dashboard-grid",
+  //   myinfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+  //   firstName: "[name='firstName']",
+  //   middleName: "[name='middleName']",
+  //   lastName: "[placeholder='Last Name']",
+  //   genericField: ".oxd-input--active",
+  //   dataField: "[placeholder='yyyy-dd-mm']",
+  //   selectArrow: ".oxd-select-text--arrow",
+  //   dateCloseButton: ".--close",
+  //   saveButton: "[type='submit']",
+  //   toastButton: ".oxd-toast",
+  //   tabIndex: "[tabindex='0']"
+    })
 
-  it('Login Fail', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userFail.loginFail)
-    cy.get(selectorList.passwordField).type(userData.userFail.passFail)
-    cy.get(selectorList.loginButton).click()
-    cy.get(selectorList.wrongCredentialAlert)
-  })
+it.only('User info update - check ', () => {
+   // visita a página de login
+   login.loginIt()
+   // insere o login e senha
+   login.loginSucess(userData.userSuccess.loginSucess,userData.userSuccess.passSucess)
+   // checa se está na pagina de dashboard
+   dashboardCheck.checkDashBoard()
+   //acessa o myinfo da pagina de dashboard e insere o formulário
+   dashboardCheck.insertForm()
+ 
 })
+  // it('Login Sucess', () => {
+  //   cy.visit('/auth/login')
+  //   cy.get(selectorList.usernameField).type(userData.userSuccess.loginSucess)
+  //   cy.get(selectorList.passwordField).type(userData.userSuccess.passSucess)
+  //   cy.get(selectorList.loginButton).click()
+  //   cy.location('pathname').should('equal' , '/web/index.php/dashboard/index')
+  //   cy.get(selectorList.checkPage)
+    
+  // })
+
+//   it('Login Fail', () => {
+//     cy.visit('/auth/login')
+//     cy.get(selectorList.usernameField).type(userData.userFail.loginFail)
+//     cy.get(selectorList.passwordField).type(userData.userFail.passFail)
+//     cy.get(selectorList.loginButton).click()
+//     cy.get(selectorList.wrongCredentialAlert)
+//   })
+// })
